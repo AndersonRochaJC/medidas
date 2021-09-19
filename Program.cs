6053414b9697689b98aef456dc3ce194e8cd211b
AndersonRochaJC <jmcreggae@gmail.com>
2021-09-19 17:35:37 -0300

    exercicio concluido

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double metros = 0;
+            
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("Digite a medida (em metros) para conversão: ");
+            metros = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\n -Conversão em Quilometros/metros/centimetros-");
+            Console.WriteLine($"{metros/1000} Km");
+            Console.WriteLine($"{metros} m");
+            Console.WriteLine($"{metros*100} cm");
+            
+            Console.ResetColor();
+           
+
+
+
+
+
+        }
+    }
+}
